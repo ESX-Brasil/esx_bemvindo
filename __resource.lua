46c85b46ff8dcfcaea4d937437fc49005c4f7a68
renildomarcio <krsolucoesweb@gmail.com>
2019-06-01 21:23:39 -0300

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Bem-vindo'
+
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
+
+dependencies {
+	'es_extended',
+	'essentialmode'
+}
